2207cabc99a405f342008b77f0c101427a9e57ef
Julia Johansson <juliajsson@hotmail.com>
2024-07-12 18:00:15 +0200

    Created initial routing and navbar for frontend.

---
+++ marquee-frontend/src/pages/UserDashboard.tsx
@@ -0,0 +1,12 @@
+const UserDashboard = () => {
+    return (
+      <div className="w-screen">
+        <h1>Användare</h1>
+        <div className="">
+
+        </div>
+      </div>
+    )
+  };
+  
+  export default UserDashboard;
