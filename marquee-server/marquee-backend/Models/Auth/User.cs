d04b7e83ed58e8f490ce3473c780820d7abf8f8c
Julia Johansson <juliajsson@hotmail.com>
2024-07-27 18:13:43 +0200

    SETUP29: Inital setup of backend and database.
    Backend: ASP.NET Core Web API template.
    
    Server: SQL Server in Docker Container

---
+++ marquee-server/marquee-backend/Models/Auth/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace marquee_backend.Models.Auth;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
---
+++ marquee-server/marquee-backend/ViewModel/Auth/Role.cs
@@ -0,0 +1,19 @@
+﻿using marquee_backend.Regex;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace marquee_backend.ViewModel.Auth
+{
+    [Table("role")]
+    public class Role
+    {
+        [SwaggerSchema(ReadOnly = true)]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("name")]
+        [RegularExpression(RegexPatterns.RoleNameRegex, ErrorMessage = "Role names may only consist out of upper-, lowercase letters, and spaces.")]
+        public required string Name { get; set; }
+    }
+}
