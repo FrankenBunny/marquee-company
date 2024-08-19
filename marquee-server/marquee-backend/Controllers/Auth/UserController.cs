58535b34634faf884881b132d10acc0d5ff987d2
Julia Johansson <juliajsson@hotmail.com>
2024-08-19 15:52:51 +0200

    INV40: Documented Inventory controller methods and adjusted methods.
    UserController.cs: Moved into Auth folder.
    ItemController.cs: Complete CRUD, duplicate titles not allowed.
    PartController.cs: Complete CRUD, duplicate titles not allowed, R(multiple) based on rentable id.
    RentableCategoryController.cs: Complete CRUD, no duplicate titles not allowed.
    RentableController.cs: Complete CRUD, duplicate titles not allowed, R(multiple) based on category id.
    RentableTagController.cs: Complete CRUD, duplicate titles not allowed.
    RentableTagRentableController.cs: CRD, no update, no duplicate objects allowed, R(single) requires id of both objects in relation, R(multiple) based on rentable id, R(multiple) based on tag id.

---
+++ marquee-server/marquee-backend/Controllers/Auth/UserController.cs
@@ -0,0 +1,78 @@
+﻿using marquee_backend.Data;
+using marquee_backend.Models.Auth;
+using marquee_backend.Models.Inventory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace marquee_backend.Controllers.Auth
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly MarqueeDatabaseContext _databaseContext;
+
+        public UserController (MarqueeDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        [HttpGet(Name = "GetUsers")]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            var users = await _databaseContext.Users.ToListAsync();
+
+            if (users == null || !users.Any())
+            {
+                return NotFound(); 
+            }  
+            
+            return users;
+        }
+
+        [HttpGet("{userId}", Name = "GetUserById")]
+        public async Task<ActionResult<User>> GetRentable (Guid userId)
+        {
+            var user = await _databaseContext.Users.FindAsync(userId);
+            if (user == null)
+                return NotFound();
+            
+            return user;
+        }
+
+        [HttpPost(Name = "AddUser")]
+        public async Task<IActionResult> AddUser (User newUser) 
+        {
+            newUser.Id = Guid.NewGuid();
+            // TODO Check that user does not already exist based on restrictions (username?)
+            _databaseContext.Users.Add(newUser);
+            await _databaseContext.SaveChangesAsync();
+
+            return Ok(newUser);
+        }
+
+        [HttpPut("{userId}", Name = "EditUser")]
+        public async Task<IActionResult> EditUser (Guid userId, User updatedUser)
+        {
+            if (userId != updatedUser.Id)
+                return BadRequest();
+            
+            _databaseContext.Entry(updatedUser).State = EntityState.Modified;
+
+            try
+            {
+                await _databaseContext.SaveChangesAsync();
+            } 
+            catch (DbUpdateConcurrencyException)
+            {
+                var user = await _databaseContext.Users.FindAsync(userId);
+                if (user == null)
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ marquee-server/marquee-backend/Controllers/Inventory/ItemController.cs
@@ -0,0 +1,112 @@
+using marquee_backend.Data;
+using marquee_backend.Models.Inventory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+/**
+ *  ItemController is responsible for CRUD operations on the Items table.
+ *  CREATE - Creates a new item, if the title does not already exist within the table.
+ *  READ (SINGLE) - Retrieves an item based on the id.
+ *  READ (MULTIPLE) - Retrieves all items within the items table.
+ *  UPDATE - Updates the items based on the given id, requires a complete object to update, where the given id matches the id in the object.
+ *  DELETE - Deletes the item based on the given id.
+ *
+ **/
+namespace marquee_backend.Controllers.Inventory
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly MarqueeDatabaseContext _databaseContext;
+
+        public ItemController (MarqueeDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> AddItem (Item newItem) 
+        {
+            newItem.Id = Guid.NewGuid();
+
+            var taken_title = _databaseContext.Items.Where(item => item.Title == newItem.Title);
+
+            if (taken_title != null)
+              return BadRequest();
+
+            _databaseContext.Items.Add(newItem);
+            await _databaseContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetItem), new {id = newItem.Id}, newItem);
+        }
+
+
+        [HttpGet("{itemId}")]
+        public async Task<ActionResult<Item>> GetItem (Guid itemId)
+        {
+          var item = await _databaseContext.Items.FindAsync(itemId);
+
+          if (item == null)
+            return NotFound();
+
+          return item;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> GetItems()
+        {
+            var items = await _databaseContext.Items.ToListAsync();
+
+            if (items == null || !items.Any())
+            {
+                return NotFound(); 
+            }  
+            
+            return items;
+        }
+
+
+        [HttpPut("{itemId}")]
+        public async Task<IActionResult> EditItem (Guid itemId, Item updatedItem)
+        {
+          if (itemId != updatedItem.Id)
+            return BadRequest();
+
+          _databaseContext.Entry(updatedItem).State = EntityState.Modified;
+
+          try
+          {
+            await _databaseContext.SaveChangesAsync();
+          }
+          catch (DbUpdateConcurrencyException)
+          {
+            var item = await _databaseContext.Items.FindAsync(itemId);
+            if (item == null)
+              return NotFound();
+            else
+            {
+              throw;
+            }
+          }
+
+          return NoContent();
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveItem (Guid itemId)
+        {
+          var toBeRemoved = await _databaseContext.Items.FindAsync(itemId);
+          if (toBeRemoved == null)
+            return NotFound();
+
+          _databaseContext.Items.Remove(toBeRemoved);
+          await _databaseContext.SaveChangesAsync();
+
+          return NoContent();
+        }
+    }
+}
